8401422f7216bd1e0c10e3fb2403bfea3618c8ca
Veggie <cmderochie+github@gmail.com>
2017-01-26 00:57:11 -0700

    Initial commit.

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    public string TablePlayer;
+    public string ControllerName;
+
+    //const float Extent1Bar;
+    //const float Extent2Bar;
+    //const float Extent5Bar;
+    //const float Extent3Bar;
+
+    GameObject _1bar;
+    GameObject _2bar;
+    GameObject _5bar;
+    GameObject _3bar;
+
+    GameObject[] _bars;
+
+    int _left, _right;
+
+	// Use this for initialization
+	void Start () {
+        _1bar = GameObject.Find(TablePlayer + "1Bar");
+        _2bar = GameObject.Find(TablePlayer + "2Bar");
+        _5bar = GameObject.Find(TablePlayer + "5Bar");
+        _3bar = GameObject.Find(TablePlayer + "3Bar");
+
+        _bars = new[] { _1bar, _2bar, _5bar, _3bar };
+
+        _left = 0;
+        _right = 2;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        updateBar(_left, "Left");
+        updateBar(_right, "Right");
+    }
+
+    void updateBar(int bar, string controlName)
+    {
+        var barPosition = _bars[bar].transform.localPosition;
+        barPosition.z = 0.375f * Input.GetAxis(ControllerName + "_" + controlName + "Y");
+        _bars[bar].transform.localPosition = barPosition;
+
+        var barRotation = _bars[bar].transform.localRotation;
+        barRotation.z = Mathf.PI * 0.25f * Input.GetAxis(ControllerName + "_" + controlName + "X");
+        _bars[bar].transform.rotation = barRotation;
+    }
+}
---
+++ Assets/Scripts/TableScale.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableScale : MonoBehaviour {
+    public float TableLength;
+    public float TableWidth;
+    public float TableDepth;
+    public float BarHeightPortion;
+    public float ManLength;
+
+	// Use this for initialization
+	void Start () {
+        var cabinet = GameObject.Find("Cabinet");
+        var bars = GameObject.Find("Bars");
+
+        cabinet.transform.localScale = new Vector3(TableLength, TableDepth, TableWidth);
+
+        var barsPosition = bars.transform.localPosition;
+        barsPosition.y = BarHeightPortion * TableDepth;
+        bars.transform.localPosition = barsPosition;
+
+        var barsScale = bars.transform.localScale;
+        barsScale.z = TableWidth;
+        bars.transform.localScale = barsScale;
+
+        foreach (Transform childTransform in bars.transform)
+        {
+            var child = childTransform.gameObject;
+            var childPosition = childTransform.localPosition;
+            childPosition.x *= TableLength;
+            childTransform.localPosition = childPosition;
+
+            foreach (Transform manTransform in childTransform)
+            {
+                if (manTransform.gameObject.name.EndsWith("Rod")) continue;
+                var manScale = manTransform.localScale;
+                manScale.y = ManLength;
+                manTransform.localScale = manScale;
+
+                var manPosition = manTransform.localPosition;
+                manPosition.y = 0.015f - ManLength / 2f;
+                manTransform.localPosition = manPosition;
+            }
+        }
+	}
+
+}
